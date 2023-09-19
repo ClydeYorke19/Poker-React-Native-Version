15405bd9089786c66c85690c36871521a3440548
ClydeYorke19 <92651370+ClydeYorke19@users.noreply.github.com>
2023-09-19 12:27:34 -0400

    Add files via upload

--- client/models/UserModel.js
+++ client/models/UserModel.js
@@ -10,6 +10,10 @@ class User {
     gameId;
     friendRequests = [];
     friendsList = [];
+    groups = {}
+    groupNames = []
+    groupRequests = [];
+    alerts = [];
 
     constructor(id, status, loggedIn, accountInfo, inGame, playerGameObject, socket) {
         this.id = id
@@ -20,10 +24,39 @@ class User {
         this.socket = socket
     }
 
+    addAlert(type, sender, aInfo) {
+
+        if (type === 'friend_request') {
+
+            if (!this.friendRequests.includes(sender) && !this.friendsList.includes(sender)) {
+                this.alerts.push({'type': 'friend_request', 'sender': sender})
+
+            }
+
+        } else if (type === 'group_request') {
+            if (!this.groupRequests.includes(sender) && !this.groupNames.includes(aInfo)) {
+                this.alerts.push({'type': 'group_request', 'sender': sender, 'groupName': aInfo})
+            }
+
+        }
+
+    }
+
     addFriendToList(friendUsername) {
         if (!this.friendsList.includes(friendUsername)) {
             this.friendsList.push(friendUsername);
         }
+    }
+
+    addGroup(groupInfo) {
+        if (!this.groupNames.includes(groupInfo.name)) {
+            this.groups[groupInfo.name] = groupInfo;
+            this.groupNames.push(groupInfo.name)
+        }
+    }
+
+    updateGroupInfo(groupInfo) {
+        this.groups[groupInfo.groupName] = groupInfo;
     }
 
 
